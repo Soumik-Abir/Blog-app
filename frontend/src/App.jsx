7aff4bb3272b272ab9fce0c6a50155fb3d1b7369
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2024-03-05 02:16:04 +0530

    Add tailwind-scrollbar plugin and update index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,9 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
 
 function App() {
   return (
@@ -21,6 +24,10 @@ function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route element={<PrivateRoute/>}>
           <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
         </Route>
         <Route path='/projects' element={<Projects />} />
 
