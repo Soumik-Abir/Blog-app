8bc13a51ac76f52d5d5e2ad6cd1e2c5a50529499
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2024-03-04 10:48:22 +0530

    Add nodemailer dependency and update server configuration

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,31 @@
 import express from "express";
 import connectDB from "./db/connectDB.js";
 import dotenv from "dotenv";
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
 app.listen(PORT, () => {
     connectDB();
 	console.log(`Server Running on port ${PORT}`);
 });
+
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
