6879191bda7b99e4a2da1a37bb7389cac9b07927
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2024-03-05 11:43:22 +0530

    Add CallToAction component and update Footer and index.css

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Sahand's
+                Soumik's
               </span>
               Blog
             </Link>
@@ -33,7 +33,7 @@ export default function FooterCom() {
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Sahand's Blog
+                  Soumik's Blog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -41,7 +41,7 @@ export default function FooterCom() {
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
+                  href='https://www.github.com/Soumik-Abir'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
